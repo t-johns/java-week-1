b018aa052df1ecc7260b2d7cbd0927cc10a61fc8
Trevor <trjohnson.1994@gmail.com>
2023-01-06 15:59:23 -0500

    Add .settings to .gitignore

--- src/week1/demo/Week1Demo.java
+++ src/week1/demo/Week1Demo.java
@@ -3,8 +3,7 @@ package week1.demo;
 public class Week1Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	  System.out.println("Hi from the class!");
 	}
 
 }
